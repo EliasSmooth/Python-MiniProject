b77b756b385c656790c30c8cf90d97e831dfba73
Eliadad <eliasashtonsaunders@gmail.com>
2022-02-12 15:58:47 -0700

    finished the data removal and storage, tomorrow I am going to work on getting the data properly stored in a log

--- WriteToLog.py
+++ WriteToLog.py
@@ -1,27 +1,38 @@
 #Define function to read and write to the log
-import openpyxl
+from fnmatch import fnmatch
+from openpyxl import Workbook, load_workbook
 import glob
 import csv
 
 files = glob.glob('./Assets/*.xlsx')
-data = []
+
+sumdata = []
+vocdata = []
+monthdata = []
 
 months = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"}
 
 #Creates the CSV file needed for reading
-def writeCSV(sheet):
-    with open('test.csv', 'w', newline="") as file_handle:
+def writeCSV(sheet, name):
+    with open('{}.csv'.format(name), 'w', newline="") as file_handle:
         csv_writer = csv.writer(file_handle)
-        for row in sheet.iter_rows(): 
-            csv_writer.writerow([cell.value for cell in row])
+        for row in sheet.iter_rows():
+            try: 
+                csv_writer.writerow([cell.value for cell in row])
+            except:
+                pass
+
 
 #Process derives the proper date formating and matches it to the applicable data
 def getData(file): 
-    wb = openpyxl.load_workbook(file)
-    sh = wb.active
-    
+    wp = load_workbook(file)
+    wp1 = wp["Summary Rolling MoM"] 
+    wp2 = wp["VOC Rolling MoM"]
+    wp3 = wp["Monthly Verbatim Statements"]
+
     month = file[32:35]
     month = month[0].upper() + month[1:]
+    year = file[38:42]
 
     def monthMatch(month):
         for i in months:
@@ -29,22 +40,53 @@ def getData(file):
                 return (months[month])
             else: pass
 
-    year = file[38:42]
     formatted = year + "-" + monthMatch(month)
 
-    writeCSV(sh)
+    def readCSV(number):
+            with open("{}.csv".format(number), 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, dialect="excel")
+                if wp.active == wp1:
+                    for line in csv_reader:
+                        if line[0][0:7] == formatted:
+                            print("sucess")
+                            for x in line:
+                                if x != '' or 0:
+                                    sumdata.append(x)
+                                    print(sumdata)
+                if wp.active == wp2:
+                    for line in csv_reader:          
+                        for x in line[0]:
+                            var = x[0:7]
+                            if var == formatted:       
+                                vocdata.append(line[x])
+                if wp.active == wp3:
+                    for line in csv_reader:
+                        if line[0][0:7] == formatted:
+                            for x in line:
+                                monthdata.append(x)
+                        
+                            
+
+    if wp.active == wp1:
+        writeCSV(wp1, "1")
+        readCSV("1")
+
+    wp.active = wp2 
+
+    if wp.active == wp2:
+        writeCSV(wp2, "2")
+        readCSV("2")
+        
+    wp.active = wp3
+ 
+    if wp.active == wp3:
+        writeCSV(wp3, "3")
+        readCSV("3")
+
+    
 
     #Next process processes the excel and turns it into iterable CSV
-    with open("test.csv", 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, dialect="excel")
-        for line in csv_reader:
-            print(line[0][0:7])
-            if line[0][0:7] == formatted:
-                print("sucess")
-                for x in line:
-                    if x != '' or 0:
-                        data.append(x)
-                        print(data)
+    
 
 
 
